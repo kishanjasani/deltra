281814a04f25e302f3fb001883b9d2b45c56c19c
Kishan Jasani <kishanjasani007@yahoo.in>
2025-03-10 23:06:01 +0530

    Add default query pattern and add it to index page

---
+++ patterns/default-query-grid.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Default posts grid
+ * Slug: deltra/default-query-grid
+ * Categories: query
+ * Block Types: core/query
+ * Description: A list of posts with featured image and post date.
+ *
+ * @package    KishanJasani
+ * @subpackage deltra
+ * @since      deltra 1.0
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Default posts grid"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)">
+	<!-- wp:query {"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"enhancedPagination":true,"align":"wide"} -->
+	<div class="wp-block-query alignwide">
+		<!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"grid","columnCount":2}} -->
+		<!-- wp:group {"style":{"border":{"radius":"8px","width":"1px"}},"borderColor":"accent-8","layout":{"type":"constrained"}} -->
+		<div class="wp-block-group has-border-color has-accent-8-border-color" style="border-width:1px;border-radius:8px">
+			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"0px","bottomRight":"0px"}}}} /-->
+
+			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium","padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|large","left":"var:preset|spacing|large","right":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--large)">
+				<!-- wp:post-date {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-5"}}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"accent-5","fontSize":"x-small"} /-->
+				<!-- wp:post-title {"isLink":true} /-->
+
+				<!-- wp:post-excerpt {"moreText":"Continue Reading","excerptLength":30} /-->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+		<!-- /wp:post-template -->
+
+		<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--large)">
+			<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+				<!-- wp:query-pagination-previous /-->
+
+				<!-- wp:query-pagination-numbers {"midSize":1} /-->
+
+				<!-- wp:query-pagination-next /-->
+			<!-- /wp:query-pagination -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:query-no-results -->
+			<!-- wp:paragraph {"align":"center","placeholder":"Add text or blocks that will display when a query returns no results."} -->
+			<p class="has-text-align-center"><?php echo esc_html_x( 'Sorry, but nothing was found. Please try a search with different keywords.', 'Message explaining that there are no results returned from a search.', 'deltra' ); ?></p>
+			<!-- /wp:paragraph -->
+		<!-- /wp:query-no-results -->
+	</div>
+	<!-- /wp:query -->
+</div>
+<!-- /wp:group -->
