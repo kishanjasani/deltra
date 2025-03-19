5325a12a31c4acff26a4eb2c784b28ac9399ee29
Kishan Jasani <kishanjasani007@yahoo.in>
2025-03-19 10:13:01 +0530

    Add Featured section pattern with logos

---
+++ patterns/social-logo.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Title:Social - Logos
+ * Slug: deltra/social-logos
+ * Categories: social-logos
+ * Description: Social logo like Facebook, WordPress, GitHub.
+ *
+ * @package KishanJasani
+ * @subpackage deltra
+ * @since deltra 1.0
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"},"metadata":{"name":"Social - Logos"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"13px"}},"textColor":"accent-6", "fontSize":"medium"} -->
+		<p class="has-text-align-center has-accent-6-color has-text-color has-medium-font-size"><?php esc_html_e( 'Featured In', 'deltra' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+		<div class="wp-block-group alignwide">
+			<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"0px"},"layout":{"selfStretch":"fit","flexSize":null},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}}} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border" id="wp-duotone-dark-deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group alignwide">
+				<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"},"color":{"duotone":"var:preset|duotone|dark"}},"className":"wp-duotone-dark deltra-icon"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border wp-duotone-dark deltra-icon"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/deltra.png" alt="" style="border-radius:0px;width:100px"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
