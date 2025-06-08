84f0e285d053d59f44d09e1e4928ec60db2501e9
Kishan Jasani <kishanjasani007@yahoo.in>
2025-06-08 09:45:36 +0530

    Register Button block style

---
+++ app/Deltra.php
@@ -0,0 +1,43 @@
+<?php
+namespace App;
+
+class Deltra {
+
+	protected array $services = [];
+
+	/**
+	 * Initializes the Deltra theme services.
+	 *
+	 * This constructor registers all necessary services for the theme.
+	 *
+	 * @since Deltra 1.0
+	 */
+	public function __construct() {
+		$this->register_services();
+	}
+
+	/**
+	 * Registers all services for the Deltra theme.
+	 *
+	 * @since Deltra 1.0
+	 *
+	 * @return void
+	 */
+	protected function register_services() {
+		// Future services can be added here.
+		$services = [
+			\App\Services\BlockStyleRegister::class,
+			\App\Services\BlockStyleAssets::class
+		];
+
+		foreach ( $services as $service_class ) {
+			$instance = new $service_class();
+
+			if ( method_exists( $instance, 'register' ) ) {
+				$instance->register();
+			}
+
+			$this->services[ $service_class ] = $instance;
+		}
+	}
+}
---
+++ app/services/BlockStyleRegister.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Services;
+
+class BlockStyleRegister {
+
+	/**
+	 * Registers the service to register custom block styles.
+	 *
+	 * This method hooks into the 'init' action to register custom block styles
+	 * for the theme.
+	 *
+	 * @since Deltra 1.0
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_action( 'init', [ $this, 'register_block_styles' ] );
+	}
+
+	/**
+	 * Registers custom block styles for the theme.
+	 *
+	 * This method registers custom styles for the 'core/button' block,
+	 * allowing for different button styles to be applied in the block editor.
+	 *
+	 * @since Deltra 1.0
+	 *
+	 * @return void
+	 */
+	public function register_block_styles() {
+		$block_styles = [
+			'core/button' => [
+				'button-secondary'   => __( 'Secondary', 'deltra' ),
+				'button-tertiary'    => __( 'Tertiary', 'deltra' ),
+				'button-foreground'  => __( 'Foreground', 'deltra' ),
+				'button-background'  => __( 'Background', 'deltra' ),
+				'button-text'        => __( 'Text', 'deltra' ),
+			],
+		];
+
+		foreach ( $block_styles as $block => $styles ) {
+			foreach ( $styles as $style_name => $style_label ) {
+				register_block_style(
+					$block,
+					[
+						'name'  => $style_name,
+						'label' => $style_label,
+					]
+				);
+			}
+		}
+	}
+}
